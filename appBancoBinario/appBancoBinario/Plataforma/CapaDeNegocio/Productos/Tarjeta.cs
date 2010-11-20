91958b7d4c78766334d3cbd857aa0d6ed115e50f
gerardo.camilo <gerardo.camilo@d7bdaa60-e54a-f9f9-872b-daa4630ed422>
2010-11-20 15:37:53 +0000

    Plataforma/CapaDeDatos/PlataformaDAO.cs | Plataforma/CapaDeNegocio/Productos/Tarjeta.cs Agregando EnvioTarjeta y RecibirTarjeta. Y mappeando correctamente los valores de los parametros en pCrearSolicitudTarjeta

--- appBancoBinario/appBancoBinario/Plataforma/CapaDeNegocio/Productos/Tarjeta.cs
+++ appBancoBinario/appBancoBinario/Plataforma/CapaDeNegocio/Productos/Tarjeta.cs
@@ -7,56 +7,70 @@ namespace appBancoBinario.Plataforma.CapaDeNegocio.Productos
 {
     public class Tarjeta : Producto
     {
-        private String sNumeroTarjeta;
-        private String sTipoTrarjeta;
-        private String sEstado;
-        private DateTime dValidarDesde;
-        private DateTime dValidarHasta;
-        private String sPIN;
-        private String sCuentaAsociada;
-        private float fLimiteCredito;
+        private String _sNumeroTarjeta;
+        private String _sTipoTrarjeta;
+        private String _sEstado;
+        private DateTime _dValidarDesde;
+        private DateTime _dValidarHasta;
+        private String _sPIN;
+        private String _sCuentaAsociada;
+        private float _fLimiteCredito;
+        private String _sRecibirTarjeta;
+        private String _sEnvioTarjeta;
+
+        public String EnvioTarjeta
+        {
+            get { return _sEnvioTarjeta; }
+            set { _sEnvioTarjeta = value; }
+        }
+
+        public String RecibirTarjeta
+        {
+            get { return _sRecibirTarjeta; }
+            set { _sRecibirTarjeta = value; }
+        }
 
         public String CuentaAsociada
         {
-            get { return sCuentaAsociada; }
-            set { sCuentaAsociada = value; }
+            get { return _sCuentaAsociada; }
+            set { _sCuentaAsociada = value; }
         }
         
 
         public float LimiteCredito
         {
-            get { return fLimiteCredito; }
-            set { fLimiteCredito = value; }
+            get { return _fLimiteCredito; }
+            set { _fLimiteCredito = value; }
         }
 
         public String NumeroTarjeta
         {
-            get { return sNumeroTarjeta; }
-            set { sNumeroTarjeta = value; }
+            get { return _sNumeroTarjeta; }
+            set { _sNumeroTarjeta = value; }
         }
         
         public String TipoTrarjeta
         {
-            get { return sTipoTrarjeta; }
-            set { sTipoTrarjeta = value; }
+            get { return _sTipoTrarjeta; }
+            set { _sTipoTrarjeta = value; }
         }
         
         public String Estado
         {
-            get { return sEstado; }
-            set { sEstado = value; }
+            get { return _sEstado; }
+            set { _sEstado = value; }
         }
         
         public DateTime ValidarDesde
         {
-            get { return dValidarDesde; }
-            set { dValidarDesde = value; }
+            get { return _dValidarDesde; }
+            set { _dValidarDesde = value; }
         }
         
         public DateTime ValidarHasta
         {
-            get { return dValidarHasta; }
-            set { dValidarHasta = value; }
+            get { return _dValidarHasta; }
+            set { _dValidarHasta = value; }
         }
       
         public String PIN
